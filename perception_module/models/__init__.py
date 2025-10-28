e66b7255491b47626f8045861e4f51017e5db510
Shubham Pawar <smp3@stir.ac.uk>
2025-10-28 15:40:03 +0000

    new file stracture

--- perception_module/models/__init__.py
+++ perception_module/models/__init__.py
@@ -1,12 +1,12 @@
-from .model import (
+from .models.model import (
     PerceptionModel, 
     load_compatible_weights,
     get_optimizer_scheduler,
     freeze_backbone,
     unfreeze_backbone
 )
-from .backbones import get_backbone
-from .loss import WeightedMSELoss
+from .models.backbones import get_backbone
+from .models.loss import WeightedMSELoss
 
 __all__ = [
     'PerceptionModel',
